fb84621ec18043af01a57426205594910f643cab
Mathusan Selvarajah <mathusan@google.com>
2025-02-13 00:04:13 +0000

    add next config to add cache-control

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,27 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [{ protocol: "https", hostname: "storage.googleapis.com",  pathname: '**',}],
+    remotePatterns: [
+      { protocol: "https", hostname: "storage.googleapis.com", pathname: "**" },
+    ],
+  },
+  output: "standalone",
+  async headers() {
+    return [
+      {
+        source: "/(_next/static.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store",
+          },
+          {
+            key: "MATHUSAN_HEADER",
+            value: "YES",
+          },
+        ],
+      },
+    ];
   },
 };
 
