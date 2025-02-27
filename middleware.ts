045d5abab97358eb9fe7f08a7867dcd7c1e8447a
Mathusan Selvarajah <mathusan@google.com>
2025-02-27 18:21:01 +0000

    force build

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,7 @@ import type { NextRequest } from "next/server";
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   console.log("Hello from middleware");
+  console.log("Bye from middlware");
 }
 
 // See "Matching Paths" below to learn more
