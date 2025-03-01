6f4402221efcf1e741b2f6f6f712c8f1d70e83c1
Mathusan Selvarajah <mathusan@google.com>
2025-03-01 03:46:17 +0000

    working rewrite

--- next.config.ts
+++ next.config.ts
@@ -6,46 +6,78 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       { protocol: "https", hostname: "storage.googleapis.com", pathname: "**" },
     ],
-    unoptimized: true,
   },
   output: "standalone",
   async rewrites() {
     return {
       beforeFiles: [
-        // {
-        //   source: "/_next/image", // Match /_next/image and anything after
-        //   has: [
-        //     {
-        //       type: "query",
-        //       key: "url",
-        //       value: "(https?:\\/\\/.*)", // Ensure the URL starts with http or https
-        //     },
-        //   ],
-        //   destination: "http://:url",
-        //   basePath: false,
-        // },
-        // {
-        //   source: "/_next/image", // Match /_next/image and anything after
-        //   has: [
-        //     {
-        //       type: "query",
-        //       key: "url",
-        //       value: "https://(?<imageurl>.*)",
-        //     },
-        //   ],
-        //   destination: ":imageurl",
-        //   // destination: "/api/debug-rewrite?url=:imageurl",
-        //   basePath: false,
-        // },
-        // {
-        //   source: "/_next/image/:path*", // Match /_next/image and anything after
-        //   destination: "https://picsum.photos/200/300",
-        // },
+        {
+          source: "/_next/image",
+          has: [
+            {
+              type: "query",
+              key: "url",
+              value: "http://(?<host>.+)/(?<path>.+)",
+            },
+          ],
+          destination: "http://:host/:path",
+          basePath: false,
+        },
+        {
+          source: "/_next/image",
+          has: [
+            {
+              type: "query",
+              key: "url",
+              value: "https://(?<host>.+)/(?<path>.+)",
+            },
+          ],
+          // destination: "https://picsum.photos/200/300",
+          destination: "https://:host/:path",
+          basePath: false,
+        },
       ],
       afterFiles: [],
       fallback: [],
     };
   },
+  // async rewrites() {
+  //   return {
+  //     beforeFiles: [
+  //       // {
+  //       //   source: "/_next/image", // Match /_next/image and anything after
+  //       //   has: [
+  //       //     {
+  //       //       type: "query",
+  //       //       key: "url",
+  //       //       value: "(https?:\\/\\/.*)", // Ensure the URL starts with http or https
+  //       //     },
+  //       //   ],
+  //       //   destination: "http://:url",
+  //       //   basePath: false,
+  //       // },
+  //       // {
+  //       //   source: "/_next/image", // Match /_next/image and anything after
+  //       //   has: [
+  //       //     {
+  //       //       type: "query",
+  //       //       key: "url",
+  //       //       value: "https://(?<imageurl>.*)",
+  //       //     },
+  //       //   ],
+  //       //   destination: ":imageurl",
+  //       //   // destination: "/api/debug-rewrite?url=:imageurl",
+  //       //   basePath: false,
+  //       // },
+  //       // {
+  //       //   source: "/_next/image/:path*", // Match /_next/image and anything after
+  //       //   destination: "https://picsum.photos/200/300",
+  //       // },
+  //     ],
+  //     afterFiles: [],
+  //     fallback: [],
+  //   };
+  // },
 };
 
 export default nextConfig;
