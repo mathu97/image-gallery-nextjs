b27e1cff54ae769c7dc59a460153bb7c2c2f4bd0
Mathusan Selvarajah <mathusan@google.com>
2025-03-07 21:34:09 +0000

    add debug route

--- app/api/images/hidden/route.ts
+++ app/api/images/hidden/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
 
-export async function POST(request: Request) {
+export async function POST() {
   return NextResponse.json({ config: process.env.MATHUSAN_ADMIN_CONFIG });
 }
