e26fb22f410982c7303ce326646d633ae5b0e5d8
Mathusan Selvarajah <mathusan@google.com>
2025-03-07 21:09:32 +0000

    add debugging components

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
-import { Suspense } from "react";
+// import { Suspense } from "react";
 import { ImageGallery } from "@/components/image-gallery";
-import { PerformanceMetrics } from "@/components/performance-metrics";
-import { headers } from "next/headers";
+// import { PerformanceMetrics } from "@/components/performance-metrics";
+// import { headers } from "next/headers";
 
 export default async function Home() {
-  const allHeaders = JSON.parse(JSON.stringify(await headers())).headers;
+  // const allHeaders = JSON.parse(JSON.stringify(await headers())).headers;
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold mb-8 text-center">Image Gallery</h1>
-      <Suspense fallback={<div>Loading performance metrics...</div>}>
-        <PerformanceMetrics headers={allHeaders} />
-      </Suspense>
+      <div>
+        env var test - server: "{process.env.TEST_ENV}", client: "
+        {process.env.NEXT_PUBLIC_TEST_ENV}"
+      </div>
       <ImageGallery />
     </div>
   );
