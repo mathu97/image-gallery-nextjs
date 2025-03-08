099b482049e1e00885210f7821e54d33cf73d903
Mathusan Selvarajah <mathusan@google.com>
2025-03-08 03:58:18 +0000

    remove env vars

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,6 @@ export default async function Home() {
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold mb-8 text-center">Image Gallery</h1>
-      <div>
-        env var test - server: &quot;{process.env.TEST_ENV}&quot;, client:
-        &quot;{process.env.NEXT_PUBLIC_TEST_ENV}&quot;
-      </div>
       <ImageGallery />
     </div>
   );
