0e087710a9a7c42312b1d1658cd3c53d0d2cb7a8
Mathusan Selvarajah <mathusan@google.com>
2025-03-28 18:10:27 +0000

    add custom image loader

---
+++ loader.ts
@@ -0,0 +1,21 @@
+import type { ImageLoaderProps } from "next/image";
+
+export default function myImageLoader({
+  src,
+  width,
+  quality,
+}: ImageLoaderProps) {
+  const operations = [
+    {
+      operation: "input",
+      type: "url",
+      url: src,
+    },
+    { operation: "resize", width: width },
+    { operation: "output", format: "webp", quality: quality || 75 },
+  ];
+
+  const encodedOperations = encodeURIComponent(JSON.stringify(operations));
+
+  return `https://us-central1-fir-testing-89aa3.cloudfunctions.net/ext-image-processing-api-handler/process?operations=${encodedOperations}`;
+}
--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [{ protocol: "https", hostname: "storage.googleapis.com",  pathname: '**',}],
+    remotePatterns: [
+      { protocol: "https", hostname: "storage.googleapis.com", pathname: "**" },
+    ],
+    loader: "custom",
+    loaderFile: "./loader.ts",
   },
 };
 
