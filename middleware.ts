8a76e0a7d1ff5209f5e58a6f26b2a24fae2a0a07
Mathusan Selvarajah <mathusan@google.com>
2025-05-07 15:19:43 +0000

    update middlware to only match /static

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,9 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
-
 // This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL('/home', request.url))
+export function middleware() {
+  console.log("Middleware invoked");
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: '/about/:path*',
-}
+  matcher: "/static",
+};
